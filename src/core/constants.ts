e92844af8b48072a10eb5f4ac49ae93857b7713c
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2023-09-23 14:17:12 +0800

    fix: jsx column is 1 less (#11)

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -4,12 +4,7 @@ export const NodeTypes = <const>{
   ELEMENT: 1,
 };
 
-export const ElementTypes = <const>{
-  ELEMENT: 0,
-  COMPONENT: 1,
-};
-
-export const TagTypes = <number[]>[
-  ElementTypes.ELEMENT,
-  ElementTypes.COMPONENT,
+export const TagTypes = [
+  0, // ELEMENT
+  1, // COMPONENT
 ];
