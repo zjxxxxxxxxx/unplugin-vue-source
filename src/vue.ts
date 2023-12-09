24f53db28e531116d933c78954dc2c5e58d6483a
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2023-12-09 10:30:49 +0800

    fix: `process.env.NODE_ENV` is undefined (#19)

--- src/vue.ts
+++ src/vue.ts
@@ -1,7 +1,8 @@
 import { TRACE_ID } from './core/constants';
+import { isDev } from './core/isDev';
 export default {
   install(app: any) {
-    if (process.env.NODE_ENV === 'development') {
+    if (isDev()) {
       app.mixin({
         props: {
           [TRACE_ID]: String,
