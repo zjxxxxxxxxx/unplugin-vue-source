8f6a327d6060ee6578aa6696ca7ca2e067fe6c23
zjxxxxxxxxx <954270063@qq.com>
2024-01-02 18:14:30 +0800

    chore: Lower dependency version

--- src/core/transform_MDX.ts
+++ src/core/transform_MDX.ts
@@ -1,26 +1,22 @@
 import { type Position } from '@vue/compiler-dom';
-import { fromMarkdown } from 'mdast-util-from-markdown';
-import { mdxJsxFromMarkdown } from 'mdast-util-mdx-jsx';
-import { mdxjs } from 'micromark-extension-mdxjs';
-import { visit } from 'unist-util-visit';
+import fromMarkdown from 'mdast-util-from-markdown';
+import { fromMarkdown as mdxJsxFromMarkdown } from 'mdast-util-mdx-jsx';
+import mdxjs from 'micromark-extension-mdxjs';
+import visit from 'unist-util-visit';
 
 const mdxJsxRE = /^mdxJsx/;
 
 export function transform_MDX(code: string, cb: (pos: Position) => void) {
   const ast = fromMarkdown(code, 'utf-8', {
     extensions: [mdxjs()],
-    mdastExtensions: [mdxJsxFromMarkdown()],
+    mdastExtensions: [mdxJsxFromMarkdown],
   });
-  visit(
-    ast,
-    (node) => mdxJsxRE.test(node.type),
-    (node) => {
-      if ('name' in node && node.name != null) {
-        const { start } = node.position!;
-        const offset = start.offset! + node.name.length + 1;
+  visit(ast, (node) => {
+    if (mdxJsxRE.test(node.type) && 'name' in node && node.name != null) {
+      const { start } = node.position!;
+      const offset = start.offset! + (node.name as any).length + 1;
 
-        cb({ ...start, offset });
-      }
-    },
-  );
+      cb({ ...start, offset });
+    }
+  });
 }
