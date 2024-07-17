508605214aa5e1b1c490e88d9bffe74edca14971
zjxxxxxxxxx <954270063@qq.com>
2024-07-17 22:30:05 +0800

    fix: Normalize paths in Windows

--- src/core/index.ts
+++ src/core/index.ts
@@ -24,23 +24,27 @@ export const unpluginFactory: UnpluginFactory<Options | undefined> = (
     enforce: 'pre',
 
     transformInclude(id) {
-      const { file, query } = parse_ID(id);
+      const { file, query } = parse_ID(normalizePath(id));
       return query.raw == null && filter(file);
     },
     transform(code, id) {
-      return transform(code, id, opts, isWebpack);
+      return transform(code, normalizePath(id), opts, isWebpack);
     },
   };
 };
 
 function resolveOptions(opts: Options): ResolvedOptions {
   return {
-    root: opts.root ?? process.cwd(),
+    root: normalizePath(opts.root ?? process.cwd()),
     sourceMap: opts.sourceMap ?? false,
     babelParserPlugins: opts.babelParserPlugins ?? [],
     include: opts.include ?? /\.(vue|jsx|tsx|mdx)$/,
     exclude: opts.exclude ?? /\/node_modules\//,
   };
+}
+
+function normalizePath(path: string) {
+  return path.replace(/\\/g, '/');
 }
 
 export default /* #__PURE__ */ createUnplugin(unpluginFactory);
